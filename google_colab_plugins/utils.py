ba9fc8e591fe0c69f5986daa4bc6dbe38e9adf17
shashikg <shashikg.iitk@gmail.com>
2020-09-03 21:58:38 +0530

    bug in download util

--- google_colab_plugins/utils.py
+++ google_colab_plugins/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import subprocess
 
 def download_test_video():
-  !wget -O bunny_video.mp4 https://github.com/shashikg/google_colab_plugins/raw/master/examples/data/bunny_video.mp4
+  subprocess.check_output(["wget", "-O", "bunny_video.mp4", "https://github.com/shashikg/google_colab_plugins/raw/master/examples/data/bunny_video.mp4"])
+  print("Download Complete!")
